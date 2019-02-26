77ea6cb7ae9dc01c00b09e85c9d8e0c1ab8284c7
tonyjhuang <huangtonyj91@gmail.com>
2019-02-26 00:30:01 -0800

    Create Hand class

---
+++ src/gameObjects/Hand.js
@@ -0,0 +1,15 @@
+export default class Hand {
+  constructor() {
+    this.hand = {
+      pink: 0,
+      white: 0,
+      blue: 0,
+      yellow: 0,
+      orange: 0,
+      black: 0,
+      red: 0,
+      green: 0,
+      wild: 0,
+    }
+  }
+}
